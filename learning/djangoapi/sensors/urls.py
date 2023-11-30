83745926ab17f7136642f70abca8b90364c4bf16
diego marques <diegodesouzamarques@gmail.com>
2023-11-29 22:37:03 -0300

    create views and response

--- learning/djangoapi/sensors/urls.py
+++ learning/djangoapi/sensors/urls.py
@@ -1,7 +1,11 @@
 #admin -> sensors
 from django.urls import path, include 
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    
+    path('sensors/', views.sensors_list ),
+    path('sensors/<int:id>', views.sensors_detail)
 ]
+
+urlpatterns =  format_suffix_patterns(urlpatterns)
